1b3376402e5bef939db636354ba08e7f66e01636
Honza Bím <honzabim@gmail.com>
2018-04-11 13:59:19 +0200

    Started with the autoencoder

--- KNNmemory/anomaly_detection_ae.jl
+++ KNNmemory/anomaly_detection_ae.jl
@@ -1,4 +1,8 @@
 using Flux
 
-push!(LOAD_PATH, pwd())
+push!(LOAD_PATH, pwd(), "/home/jan/dev/anomaly detection/anomaly_detection/src", "/home/jan/dev/FluxExtensions.jl/src")
 using KNNmem
+using AnomalyDetection
+
+dataPath = "/home/jan/dev/data/loda/public/datasets/numerical"
+data = AnomalyDetection.loaddata(dataPath)
