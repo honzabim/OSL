ce9f115d11c3b9f9469d3eb05a0d219e227c1e2e
honzabim <honzabim@gmail.com>
2018-07-02 11:17:11 +0200

    Fixed implicit derivatives

--- Evaluation/implicit_vae.jl
+++ Evaluation/implicit_vae.jl
@@ -17,31 +17,14 @@ function ∇Sinv(ϵ, params...)
     dSdϕ = map(x -> Flux.Tracker.grad(x), _params)
 
     dzdϕ = map(x -> -x ./ dSdz, dSdϕ)
-
-    # foreach((x, dx) -> Tracker.@back(x, dx .* Δ), params, dzdϕ)
+    return dzdϕ
 end
 
+# It does not work with (::typeof(Sinv), Δ, ϵ, params...) for some reason...
 function Tracker.back(::typeof(Sinv), Δ, ϵ, μ, σ)
-    _z = Sinv(Flux.Tracker.data(ϵ), Flux.Tracker.data(μ), Flux.Tracker.data(σ))
-
-    _μ = Flux.Tracker.data(μ)
-    _σ = Flux.Tracker.data(σ)
-
-    _μ = param(_μ)
-    _σ = param(_σ)
-    _z = param(_z)
-
-    l = S(_z, _μ, _σ)
-    Flux.Tracker.back!(l)
-
-    dSdz = Flux.Tracker.grad(_z)
-    dSdμ = Flux.Tracker.grad(_μ)
-    dSdσ = Flux.Tracker.grad(_σ)
-
-    dzdμ = -dSdμ ./ dSdz
-    dzdσ = -dSdσ ./ dSdz
-
-    (Tracker.@back(μ, dzdμ .* Δ); Tracker.@back(σ, dzdσ .* Δ))
+    params = (μ, σ)
+    dzdϕ = ∇Sinv(ϵ, params...)
+    foreach((x, dx) -> Tracker.@back(x, dx .* Δ), params, dzdϕ)
 end
 
 mu = 0
@@ -52,7 +35,7 @@ sigma = param(sigma)
 
 e = randn()
 
-z = Sinv(mu, sigma, e)
+z = Sinv(e, mu, sigma)
 
 Flux.Tracker.back!(z)
 
