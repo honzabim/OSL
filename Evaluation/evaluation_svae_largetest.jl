a2a242d6beef6af718021f6e14b260220567cfdc
honzabim <honzabim@gmail.com>
2018-08-21 14:43:34 +0200

    added iterations

--- Evaluation/evaluation_svae_largetest.jl
+++ Evaluation/evaluation_svae_largetest.jl
@@ -5,8 +5,8 @@ using MLDataPattern
 using JLD2
 using FileIO
 
-# folderpath = "D:/dev/julia/"
-folderpath = "/home/bimjan/dev/julia/"
+folderpath = "D:/dev/julia/"
+# folderpath = "/home/bimjan/dev/julia/"
 # folderpath = "D:/dev/"
 push!(LOAD_PATH, folderpath, folderpath * "OSL/KNNmemory/")
 using KNNmem
@@ -69,75 +69,52 @@ function rscore(m::SVAE, x)
     return Flux.mse(x, xgivenz)
 end
 
-function runExperiment(datasetName, trainall, testall, createModel, anomalyCounts, batchSize = 100, numBatches = 10000)
+function runExperiment(datasetName, trainall, test, createModel, anomalyCounts, batchSize = 100, numBatches = 10000)
     anomalyRatios = [0.05, 0.01, 0.005]
     results = []
-    for ar in anomalyRatios
-        train = ADatasets.subsampleanomalous(trainall, ar)
-        subtest = ADatasets.subsampleanomalous(testall, ar)
-        test = testall
-        (model, learnRepresentation!, learnAnomaly!, classify, justTrain!) = createModel()
-        opt = Flux.Optimise.ADAM(Flux.params(model), 1e-4)
-        cb = Flux.throttle(() -> println("$datasetName AR=$ar : $(justTrain!(train[1], []))"), 5)
-        Flux.train!(justTrain!, RandomBatches((train[1], zeros(train[2]) .+ 2), batchSize, numBatches), opt, cb = cb)
-        # FluxExtensions.learn(learnRepresentation!, opt, RandomBatches((train[1], train[2] .- 1), batchSize, numBatches), ()->(), 100)
+    for i in 1:10
+        for ar in anomalyRatios
+            println("Running $datasetName with ar: $ar iteration: $i")
+            train = ADatasets.subsampleanomalous(trainall, ar)
+            (model, learnRepresentation!, learnAnomaly!, classify, justTrain!) = createModel()
+            opt = Flux.Optimise.ADAM(Flux.params(model), 1e-4)
+            cb = Flux.throttle(() -> println("$datasetName AR=$ar : $(justTrain!(train[1], []))"), 5)
+            Flux.train!(justTrain!, RandomBatches((train[1], zeros(train[2]) .+ 2), batchSize, numBatches), opt, cb = cb)
+            # FluxExtensions.learn(learnRepresentation!, opt, RandomBatches((train[1], train[2] .- 1), batchSize, numBatches), ()->(), 100)
 
-        learnRepresentation!(train[1], zeros(train[2]))
+            learnRepresentation!(train[1], zeros(train[2]))
 
-        rstrn = Flux.Tracker.data(rscore(model, train[1]))
-        rstst = Flux.Tracker.data(rscore(model, test[1]))
+            rstrn = Flux.Tracker.data(rscore(model, train[1]))
+            rstst = Flux.Tracker.data(rscore(model, test[1]))
 
-        # Train KNN
-        balltree = BallTree(Flux.Tracker.data(zfromx(model, train[1])), Euclidean(); reorder = false)
+            balltree = BallTree(Flux.Tracker.data(zfromx(model, train[1])), Euclidean(); reorder = false)
+            idxs, dists = knn(balltree, Flux.Tracker.data(zfromx(model, test[1])), 3, false)
+            knnscores = map((i, d) -> sum(softmax(1 ./ d)[train[2][i] .== 2]), idxs, dists)
+            knnauc = pyauc(test[2] .- 1, knnscores)
+            knnroc = roc(test[2] .- 1, map(i -> indmax(counts(train[2][i])) - 1, idxs))
+            knnprec = precision(knnroc)
+            knnrecall = recall(knnroc)
 
-        # Test on all test samples
-        idxs, dists = knn(balltree, Flux.Tracker.data(zfromx(model, test[1])), 3, false)
-        knnscores = map((i, d) -> sum(softmax(1 ./ d)[train[2][i] .== 2]), idxs, dists)
-        knnauc = pyauc(test[2] .- 1, knnscores)
-        knnroc = roc(test[2] .- 1, map(i -> indmax(counts(train[2][i])) - 1, idxs))
-        knnprec = precision(knnroc)
-        knnrecall = recall(knnroc)
+            anomalies = train[1][:, train[2] .- 1 .== 1] # TODO needs to be shuffled!!!
+            for ac in anomalyCounts
+                if ac <= size(anomalies, 2)
+                    l = learnAnomaly!(anomalies[:, ac], [1])
+                else
+                    println("Not enough anomalies $ac, $(size(anomalies))")
+                    println("Counts: $(counts(train[2]))")
+                    break;
+                end
 
-        # Test on subsampled test set
-        idxs, dists = knn(balltree, Flux.Tracker.data(zfromx(model, test[1])), 3, false)
-        knnscores = map((i, d) -> sum(softmax(1 ./ d)[train[2][i] .== 2]), idxs, dists)
-        subknnauc = pyauc(test[2] .- 1, knnscores)
-        knnroc = roc(test[2] .- 1, map(i -> indmax(counts(train[2][i])) - 1, idxs))
-        subknnprec = precision(knnroc)
-        subknnrecall = recall(knnroc)
+                values, probScore = classify(test[1])
+                values = Flux.Tracker.data(values)
+                probScore = Flux.Tracker.data(probScore)
 
-        anomalies = train[1][:, train[2] .- 1 .== 1] # TODO needs to be shuffled!!!
-        for ac in anomalyCounts
-            if ac <= size(anomalies, 2)
-                l = learnAnomaly!(anomalies[:, ac], [1])
-            else
-                println("Not enough anomalies $ac, $(size(anomalies))")
-                println("Counts: $(counts(train[2]))")
-                break;
+                rocData = roc(test[2] .- 1, values)
+                f1 = f1score(rocData)
+                # auc = EvalCurves.auc(EvalCurves.roccurve(probScore, test[2] .- 1)...)
+                auc = pyauc(test[2] .- 1, probScore)
+                push!(results, (ac, f1, auc, values, probScore, rstrn, rstst, knnauc, knnprec, knnrecall, ar, i))
             end
-
-            # Test on all test samples
-            values, probScore = classify(test[1])
-            # values = Flux.Tracker.data(values)
-            # probScore = Flux.Tracker.data(probScore)
-
-            rocData = roc(test[2] .- 1, values)
-            f1 = f1score(rocData)
-            # auc = EvalCurves.auc(EvalCurves.roccurve(probScore, test[2] .- 1)...)
-            auc = pyauc(test[2] .- 1, probScore)
-
-            # Test on subsampled test set
-            values, probScore = classify(test[1])
-            # values = Flux.Tracker.data(values)
-            # probScore = Flux.Tracker.data(probScore)
-
-            rocData = roc(test[2] .- 1, values)
-            subf1 = f1score(rocData)
-            # auc = EvalCurves.auc(EvalCurves.roccurve(probScore, test[2] .- 1)...)
-            subauc = pyauc(test[2] .- 1, probScore)
-
-            # push!(results, (ac, f1, auc, values, probScore, rstrn, rstst, knnauc, knnprec, knnrecall, ar))
-            push!(results, (ac, f1, auc, subauc, nothing, nothing, rstrn, rstst, knnauc, knnprec, knnrecall, subknnauc, subknnprec, subknnrecall, ar))
         end
     end
     return results
@@ -151,7 +128,7 @@ datasets = ["breast-cancer-wisconsin", "sonar", "statlog-segment"]
 difficulties = ["easy", "easy", "easy"]
 const dataPath = folderpath * "data/loda/public/datasets/numerical"
 batchSize = 100
-iterations = 100
+iterations = 10000
 
 loadData(datasetName, difficulty) =  ADatasets.makeset(ADatasets.loaddataset(datasetName, difficulty, dataPath)..., 0.8, "low")
 
--- Evaluation/postprocess_large.jl
+++ Evaluation/postprocess_large.jl
@@ -11,16 +11,17 @@ using CSV
 const dataFolder = "D:/dev/julia/OSL/experiments/WSVAElarge/"
 # const datasets = ["breast-cancer-wisconsin", "sonar", "wall-following-robot", "waveform-1", "yeast"]
 # const datasets = ["breast-cancer-wisconsin", "sonar", "wall-following-robot", "waveform-1"]
-const datasets = ["breast-cancer-wisconsin", "cardiotocography", "ecoli", "glass", "ionosphere", "musk-2", "page-blocks", "pendigits", "pima-indians", "sonar", "statlog-satimage", "statlog-segment", "waveform-1", "waveform-2", "yeast"]
+# const datasets = ["breast-cancer-wisconsin", "cardiotocography", "ecoli", "glass", "ionosphere", "musk-2", "page-blocks", "pendigits", "pima-indians", "sonar", "statlog-satimage", "statlog-segment", "waveform-1", "waveform-2", "yeast"]
+const datasets = ["breast-cancer-wisconsin", "cardiotocography", "ecoli", "glass", "ionosphere", "page-blocks", "pendigits", "pima-indians", "sonar", "statlog-satimage", "statlog-segment", "waveform-1", "waveform-2", "yeast"]
 const models = ["svae"]
 const anomalycount = 5
 
 loadExperiment(filePath) = load(filePath)["results"]
 
 # params = [:hidden, :latent, :layers, :nonlinearity, :layertype, :memorysize, :k, :anomaliesSeen, :f1, :auc, :model, :dataset]
-params = [:hidden, :latent, :layers, :nonlinearity, :layertype, :memorysize, :k, :β, :anomaliesSeen, :f1, :auc, :rsTrn, :rsTst, :knnauc, :knnprec, :knnrecall, :ar, :model, :dataset]
+params = [:hidden, :latent, :layers, :nonlinearity, :layertype, :memorysize, :k, :β, :anomaliesSeen, :f1, :auc, :subauc, :rsTrn, :rsTst, :knnauc, :knnprec, :knnrecall, :subknnauc, :subknnprec, :subknnrecall, :ar, :model, :dataset]
 # types = [Int, Int, Int, String, String, Union{Int, Missings.Missing}, Union{Int, Missings.Missing}, Int, Float64, Float64, String, String]
-types = [Int, Int, Int, String, String, Int, Int, Float64, Int, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, String, String]
+types = [Int, Int, Int, String, String, Int, Int, Float64, Int, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, String, String]
 const anomalycount = 5
 function processFile!(dataframe, model, dataset)
     println("Processing $model $dataset")
@@ -30,7 +31,7 @@ function processFile!(dataframe, model, dataset)
         for j in 1:length(results[1][2])
             pars = length(results[1][1]) > 5 ? vcat(results[i][1][1:7]..., results[i][1][9]) : vcat(results[i][1]..., -1, -1)
             # push!(dataframe, vcat(pars..., results[i][2][ac][1:3]..., model, dataset))
-            push!(dataframe, vcat(pars..., results[i][2][j][1:3]..., results[i][2][j][6:11]..., model, dataset))
+            push!(dataframe, vcat(pars..., results[i][2][j][1:3]..., results[i][2][j][6:15]..., model, dataset))
         end
     end
 end
