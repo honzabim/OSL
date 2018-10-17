28e0615b3ecd18de11a80d5daf66e622cec0e9d5
honzabim <honzabim@gmail.com>
2018-10-17 14:56:14 +0200

    Addition of the SVAE with two hyperspheres. One for anomalies and one for normal data.

---
+++ Evaluation/evaluation_svae_anom.jl
@@ -0,0 +1,143 @@
+using Adapt
+using Flux
+using MLBase: roc, f1score, precision, recall
+using MLDataPattern
+using JLD2
+using FileIO
+using FluxExtensions
+using ADatasets
+
+folderpath = "D:/dev/julia/"
+# folderpath = "/home/bimjan/dev/julia/"
+# folderpath = "D:/dev/"
+push!(LOAD_PATH, folderpath)
+using NearestNeighbors
+using StatsBase
+using InformationMeasures
+
+using PyCall
+@pyimport sklearn.metrics as sm
+
+function pyauc(labels, ascores)
+	pyfpr, pytpr, _ = sm.roc_curve(labels, ascores, drop_intermediate = true)
+	pyauc = sm.auc(pyfpr, pytpr)
+end
+
+include(folderpath * "OSL/SVAE/svae_anom.jl")
+
+
+"""
+    gridSearch(f, parameters...)
+Maps `f` to product of `parameters`.
+"""
+gridSearch(f, parameters...) = map(p -> printAndRun(f, p), Base.product(parameters...))
+
+function printAndRun(f, p)
+    println(p)
+    (p, f(p))
+end
+
+function meanpairwisemutualinf(x)
+    dim = size(x, 1)
+    mutualinf = 0
+    for i in 1:(dim - 1)
+        for j in (i + 1):dim
+            mutualinf += get_mutual_information(x[i, :], x[j, :], mode = "uniform_count")
+        end
+    end
+    return mutualinf / (dim * (dim - 1) / 2)
+end
+
+function createSVAE_anomWithMem(inputDim, hiddenDim, latentDim, numLayers, nonlinearity, layerType, memorySize, k, labelCount, β, α = 0.1, T = Float64)
+    encoder = Adapt.adapt(T, FluxExtensions.layerbuilder(inputDim, hiddenDim, hiddenDim, numLayers - 1, nonlinearity, "", layerType))
+    decoder = Adapt.adapt(T, FluxExtensions.layerbuilder(latentDim, hiddenDim, inputDim, numLayers + 1, nonlinearity, "linear", layerType))
+
+    svae = SVAE_anom(encoder, decoder, hiddenDim, latentDim, T)
+
+    learnRepresentation!(data) = wloss(svae, data, β, (x, y) -> mmd_imq(x, y, 1))
+    learnAnomaly!(anomaly) = set_anomalous_hypersphere(anomaly)
+	learnWithAnomaliesLkh!(data, labels) = wloss_anom_lkh(svae, data, β, (x, y) -> mmd_imq(x, y, 1))
+	learnWithAnomaliesWass!(data, labels) = wloss_anom_wass(svae, data, β, (x, y) -> mmd_imq(x, y, 1))
+
+    return svae, learnRepresentation!, learnAnomaly!, learnWithAnomaliesLkh!, learnWithAnomaliesWass!
+end
+
+function runExperiment(datasetName, trainall, test, createModel, anomalyCounts, batchSize = 100, numBatches = 10000, it = 1)
+    anomalyRatios = [0.05, 0.01, 0.005]
+    results = []
+    for ar in anomalyRatios
+        println("Running $datasetName with ar: $ar iteration: $it")
+        train = ADatasets.subsampleanomalous(trainall, ar)
+        (model, learnRepresentation!, learnAnomaly!, learnWithAnomaliesLkh!, learnWithAnomaliesWass!) = createModel()
+
+		opt = Flux.Optimise.ADAM(Flux.params(model), 1e-4)
+        cb = Flux.throttle(() -> println("SVAE $datasetName AR=$ar : $(learnRepresentation!(train[1]))"), 5)
+        Flux.train!(learnRepresentation!, RandomBatches((train[1], zeros(train[2]) .+ 2), batchSize, numBatches), opt, cb = cb)
+
+        a_ids = find(train[2] .- 1 .== 1)
+        a_ids = a_ids[:, randperm(size(a_ids, 2))]
+
+		# Number of batches for learning with anomalies
+		numBatches = 3000
+
+        for ac in anomalyCounts
+            if ac <= size(a_ids, 2)
+				if ac == 1
+                	l = learnAnomaly!(train[1][:, a_ids[ac]])
+				else
+					newlabels = zeros(train[2])
+					newlabels[a_ids[1:ac]] .= 1
+					opt = Flux.Optimise.ADAM(Flux.params(model), 1e-4)
+					cb = Flux.throttle(() -> println("Learning with anomalies: $(learnWithAnomaliesLkh!(train[1], newlabels))"), 5)
+					Flux.train!(learnWithAnomaliesLkh!, RandomBatches((train[1], newlabels), batchSize, numBatches), opt, cb = cb)
+				end
+            else
+                println("Not enough anomalies $ac, $(size(anomalies))")
+                println("Counts: $(counts(train[2]))")
+                break;
+            end
+
+            ascore = Flux.Tracker.data(score(model, test[1]))
+            auc = pyauc(test[2] .- 1, ascore)
+            println("AUC: $auc")
+
+            push!(results, (ac, auc, ascore, ar, it))
+        end
+    end
+    return results
+end
+
+outputFolder = folderpath * "OSL/experiments/WSVAElargeSVAEanomTest/"
+mkpath(outputFolder)
+
+# datasets = ["breast-cancer-wisconsin", "sonar", "wall-following-robot", "waveform-1"]
+# datasets = ["breast-cancer-wisconsin", "sonar", "statlog-segment"]
+datasets = ["abalone"]
+difficulties = ["easy"]
+const dataPath = folderpath * "data/loda/public/datasets/numerical"
+batchSize = 100
+iterations = 100
+
+loadData(datasetName, difficulty) =  ADatasets.makeset(ADatasets.loaddataset(datasetName, difficulty, dataPath)..., 0.8, "low")
+
+if length(ARGS) != 0
+    datasets = [ARGS[1]]
+    difficulties = ["easy"]
+end
+
+for i in 1:30
+	for (dn, df) in zip(datasets, difficulties)
+
+	    train, test, clusterdness = loadData(dn, df)
+
+	    println("$dn")
+	    println("$(size(train[2]))")
+	    println("$(counts(train[2]))")
+	    println("Running svae...")
+
+	    evaluateOneConfig = p -> runExperiment(dn, train, test, () -> createSVAEWithMem(size(train[1], 1), p...), () -> createSVAE2WithMem(size(train[1], 1), p...), 1:10, batchSize, iterations, i)
+	    results = gridSearch(evaluateOneConfig, [32], [8], [3], ["relu"], ["Dense"], [128 1024], [16], [1], [0.1])
+	    results = reshape(results, length(results), 1)
+	    save(outputFolder * dn *  "-$i-svae.jld2", "results", results)
+	end
+end
---
+++ SVAE/svae_anom.jl
@@ -0,0 +1,260 @@
+include("bessel.jl")
+
+using Flux
+using NNlib
+using Distributions
+using SpecialFunctions
+using Adapt
+using Random
+using LinearAlgebra
+
+"""
+		Implementation of Hyperspherical Variational Auto-Encoders
+
+		Original paper: https://arxiv.org/abs/1804.00891
+
+		SVAE_anom(q,g,zdim,hue,μzfromhidden,κzfromhidden)
+
+		q --- encoder - in this case encoder only encodes from input to a hidden
+						layer which is then transformed into parameters for the latent
+						layer by `μzfromhidden` and `κzfromhidden` functions
+		g --- decoder
+		zdim --- dimension of the latent space
+		hue --- Hyperspherical Uniform Entropy that is part of the KL divergence but depends only on dimensionality so can be computed in constructor
+		μzfromhidden --- function that transforms the hidden layer to μ parameter of the latent layer by normalization
+		κzfromhidden --- transforms hidden layer to κ parameter of the latent layer using softplus since κ is a positive scalar
+"""
+mutable struct SVAE_anom
+	q
+	g
+	zdim
+	hue
+	μzfromhidden
+	κzfromhidden
+	anom_priorμ
+	anom_priorκ
+
+	"""
+	SVAE_anom(q, g, hdim, zdim, T) Constructor of the S-VAE where `zdim > 3` and T determines the floating point type (default Float32)
+	"""
+	SVAE_anom(q, g, hdim::Integer, zdim::Integer, T = Float32) = new(q, g, zdim, convert(T, huentropy(zdim)), Adapt.adapt(T, Chain(Dense(hdim, zdim), x -> normalizecolumns(x))), Adapt.adapt(T, Dense(hdim, 1, softplus)), Flux.param(Adapt.adapt(T, normalize(randn(zdim)))), Flux.param(Adapt.adapt(T, [1.])))
+end
+
+Flux.@treelike(SVAE_anom)
+
+normalizecolumns(m) = m ./ sqrt.(sum(m .^ 2, dims = 1) .+ eps(eltype(Flux.Tracker.data(m))))
+
+"""
+	vmfentropy(m, κ)
+
+	Entropy of Von Mises-Fisher distribution
+"""
+vmfentropy(m, κ) = .-κ .* besselix(m / 2, κ) ./ besselix(m / 2 - 1, κ) .- ((m ./ 2 .- 1) .* log.(κ) .- (m ./ 2) .* log(2π) .- (κ .+ log.(besselix(m / 2 - 1, κ))))
+
+"""
+	huentropy(m)
+
+	Entropy of Hyperspherical Uniform distribution
+"""
+huentropy(m) = m / 2 * log(π) + log(2) - lgamma(m / 2)
+
+"""
+	kldiv(model::SVAE_anom, κ)
+
+	KL divergence between Von Mises-Fisher and Hyperspherical Uniform distributions
+"""
+kldiv(model::SVAE_anom, κ) = .- vmfentropy(model.zdim, κ) .+ model.hue
+
+"""
+	loss(m::SVAE_anom, x)
+
+	Loss function of the S-VAE combining reconstruction error and the KL divergence
+"""
+function loss(m::SVAE_anom, x, β)
+	(μz, κz) = zparams(m, x)
+	z = samplez(m, μz, κz)
+	xgivenz = m.g(z)
+	return Flux.mse(x, xgivenz) + β * mean(kldiv(m, κz))
+end
+
+function pairwisecos(x, y)
+	m = x' * y .* (1 - eps(Float32) * size(x, 1))
+	acos.(m)
+end
+pairwisecos(x) = pairwisecos(x, x)
+
+function samplehsuniform(size...)
+	v = randn(size...)
+	v = normalizecolumns(v)
+end
+
+k_imq(x,y,c) = sum( c./ (c .+ pairwisecos(x,y))) / (size(x,2) * size(y,2))
+k_imq(x::T,c) where {T<:AbstractMatrix} = sum(c ./ (c .+ pairwisecos(x)))/(size(x,2) * (size(x,2) -1 ))
+k_imq(x::T,c) where {T<:AbstractVector} = zero(eltype(x))
+
+mmd_imq(x,y,c) = k_imq(x,c) + k_imq(y,c) - 2 * k_imq(x,y,c)
+
+log_normal(x) = - sum((@. x ^ 2), 1) / 2 - size(x, 1) * log(2π) / 2
+log_normal(x, μ) = log_normal(x - μ)
+
+# Likelihood estimation of a sample x under VMF with given parameters taken from https://pdfs.semanticscholar.org/2b5b/724fb175f592c1ff919cc61499adb26996b1.pdf
+# normalizing constant for density function of VMF
+c(p, κ) = κ ^ (p / 2 - 1) / ((2π) ^ (p / 2) * besseli(p / 2 - 1, κ))
+
+# log likelihood of one sample under the VMF dist with given parameters
+log_vmf(x, μ, κ) = κ * μ' * x .+ log(c(length(μ), κ))
+
+# Effective sample size
+neff(weights) = sum(weights) ^ 2 / (sum(weights .^ 2))
+
+function px(m::SVAE_anom, x::Matrix, k::Int = 100)
+	x = [x[:, i] for i in 1:size(x, 2)]
+	return map(a -> px(m, a, k), x)
+end
+
+function px(m::SVAE_anom, x::Vector, k::Int = 100)
+	μz, κz = zparams(m, x)
+	μz = repmat(Flux.Tracker.data(μz), 1, k)
+	κz = repmat(Flux.Tracker.data(κz), 1, k)
+	z = Flux.Tracker.data(samplez(m, μz, κz))
+	xgivenz = Flux.Tracker.data(m.g(z))
+
+	pxgivenz = log_normal(xgivenz, repmat(x, 1, k))
+	pz = log_vmf(z, m.priorμ, m.priorκ[1])
+	qzgivenx = log_vmf(z, μz[:, 1], κz[1])
+
+	ess = neff(exp.(pz .- qzgivenx))
+	println("Effective sample size: $ess")
+
+	return log(sum(exp.(Flux.Tracker.data(pxgivenz .+ pz .- qzgivenx))))
+end
+
+function set_anomalous_hypersphere(m::SVAE_anom, anomaly)
+	μz, _ = zparams(m, anomaly)
+	κz = 1000
+	m.anom_priorμ = μz
+	m.anom_priorκ = κz
+end
+
+function wloss(m::SVAE_anom, x, β, d)
+	(μz, κz) = zparams(m, x)
+	z = samplez(m, μz, κz)
+	zp = samplehsuniform(size(z))
+	#prior = samplez(m, ones(size(μz)) .* normalizecolumns(m.priorμ), ones(size(κz)) .* m.priorκ)
+	Ω = d(z, zp)
+	xgivenz = m.g(z)
+	return Flux.mse(x, xgivenz) + β * Ω
+end
+
+function wloss_anom_lkh(m::SVAE_anom, x, y, β, d)
+	(μz, κz) = zparams(m, x)
+	z = samplez(m, μz, κz)
+	zp = samplehsuniform(size(z))
+	anom_prior = samplez(m, ones(size(μz)) .* normalizecolumns(m.anom_priorμ), ones(size(κz)) .* m.anom_priorκ)
+	Ω = d(z, zp)
+	Ωanom = d(z, anom_prior)
+	xgivenz = m.g(z)
+	return (1 .- y) .* (Flux.mse(x, xgivenz) + β * Ω) .+ y .* (-log_vmf(z, m.anom_priorμ, m.anom_priorκ))
+end
+
+function wloss_anom_wass(m::SVAE_anom, x, y, β, d)
+	(μz, κz) = zparams(m, x)
+	z = samplez(m, μz, κz)
+	zp = samplehsuniform(size(z))
+	anom_prior = samplez(m, ones(size(μz)) .* normalizecolumns(m.anom_priorμ), ones(size(κz)) .* m.anom_priorκ)
+	Ω = d(z, zp)
+	Ωanom = d(z, anom_prior)
+	xgivenz = m.g(z)
+	return (1 .- y) .* (Flux.mse(x, xgivenz) + β * Ω) .+ y .* β .* Ωanom
+end
+
+score(m::SVAE_anom, x) = -log_vmf(zfromx(m, x), m.anom_priorμ, m.anom_priorκ)
+
+"""
+	infer(m::SVAE_anom, x)
+
+	infer latent variables and sample output x
+"""
+function infer(m::SVAE_anom, x)
+	(μz, κz) = zparams(m, x)
+	z = samplez(m, μz, κz)
+	xgivenz = m.g(z)
+	return xgivenz
+end
+
+"""
+	zparams(model::SVAE_anom, x)
+
+	Computes μ and κ from the hidden layer
+"""
+function zparams(model::SVAE_anom, x)
+	hidden = model.q(x)
+	return model.μzfromhidden(hidden), model.κzfromhidden(hidden)
+end
+
+"""
+	zfromx(m::SVAE_anom, x)
+
+	convenience function that returns latent layer based on the input `x`
+"""
+zfromx(m::SVAE_anom, x) = samplez(m, zparams(m, x)...)
+
+"""
+	samplez(m::SVAE_anom, μz, κz)
+
+	samples z layer based on its parameters
+"""
+function samplez(m::SVAE_anom, μz, κz)
+	ω = sampleω(m, κz)
+	normal = Normal()
+	v = Adapt.adapt(eltype(Flux.Tracker.data(κz)), rand(normal, size(μz, 1) - 1, size(μz, 2)))
+	v = normalizecolumns(v)
+	z = householderrotation(vcat(ω, sqrt.(1 .- ω .^ 2) .* v), μz)
+	return z
+end
+
+"""
+	householderrotation(zprime, μ)
+
+	uses Householder reflection to rotate the `zprime` vector according to mapping of e1 to μ
+"""
+function householderrotation(zprime, μ)
+	e1 = similar(μ) .= 0
+	e1[1, :] .= 1
+	u = e1 .- μ
+	normalizedu = normalizecolumns(u)
+	return zprime .- 2 .* sum(zprime .* normalizedu, dims = 1) .* normalizedu
+end
+
+function sampleω(model::SVAE_anom, κ)
+	m = model.zdim
+	c = @. sqrt(4κ ^ 2 + (m - 1) ^ 2)
+	b = @. (-2κ + c) / (m - 1)
+	a = @. (m - 1 + 2κ + c) / 4
+	d = @. (4 * a * b) / (1 + b) - (m - 1) * log(m - 1)
+	ω = rejectionsampling(m, a, b, d)
+	return ω
+end
+
+function rejectionsampling(m, a, b, d)
+	beta = Beta((m - 1) / 2, (m - 1) / 2)
+	T = eltype(Flux.Tracker.data(a))
+	ϵ, u = Adapt.adapt(T, rand(beta, size(a)...)), Adapt.adapt(T, rand(size(a)))
+
+	accepted = isaccepted(ϵ, u, m, Flux.data(a), Flux.Tracker.data(b), Flux.data(d))
+	while !all(accepted)
+		mask = .! accepted
+		ϵ[mask] = Adapt.adapt(T, rand(beta, sum(mask)))
+		u[mask] = Adapt.adapt(T, rand(sum(mask)))
+		ia = isaccepted(mask, ϵ, u, m, Flux.data(a), Flux.data(b), Flux.data(d))
+		accepted[mask] = ia
+	end
+	return @. (1 - (1 + b) * ϵ) / (1 - (1 - b) * ϵ)
+end
+
+isaccepted(mask, ϵ, u, m:: Int, a, b, d) = isaccepted(ϵ[mask], u[mask], m, a[mask], b[mask], d[mask]);
+function isaccepted(ϵ, u, m:: Int, a, b, d)
+	ω = @. (1 - (1 + b) * ϵ) / (1 - (1 - b) * ϵ)
+	t = @. 2 * a * b / (1 - (1 - b) * ϵ)
+	@. (m - 1) * log(t) - t + d >= log(u)
+end
