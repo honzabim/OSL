1f6a3bff1fc8940c4129d9fd6ccf38beae07c0ce
Jan Bim <bimjan@bayes.felk.cvut.cz>
2018-11-06 08:38:32 +0100

    chmods

--- Evaluation/runlarge.sh
+++ Evaluation/runlarge.sh
@@ -1,33 +1,33 @@
-julia evaluation_svae_px_score.jl breast-cancer-wisconsin &
+julia evaluation_svae_anom.jl breast-cancer-wisconsin &
 sleep 40
-julia evaluation_svae_px_score.jl cardiotocography &
+julia evaluation_svae_anom.jl cardiotocography &
 sleep 40
-julia evaluation_svae_px_score.jl ecoli &
-sleep 40
-julia evaluation_svae_px_score.jl glass &
-sleep 40
-# julia evaluation_svae_px_score.jl magic-telescope &
+#julia evaluation_svae_anom.jl ecoli &
+#sleep 40
+#julia evaluation_svae_anom.jl glass &
+#sleep 40
+# julia evaluation_svae_anom.jl magic-telescope &
 # sleep 40
-julia evaluation_svae_px_score.jl musk-2 &
+julia evaluation_svae_anom.jl musk-2 &
 sleep 40
-julia evaluation_svae_px_score.jl page-blocks &
+julia evaluation_svae_anom.jl page-blocks &
 sleep 40
-julia evaluation_svae_px_score.jl pendigits &
+#julia evaluation_svae_anom.jl pendigits &
+#sleep 40
+#julia evaluation_svae_anom.jl pima-indians &
+#sleep 40
+#julia evaluation_svae_anom.jl sonar &
+#sleep 40
+julia evaluation_svae_anom.jl statlog-satimage &
 sleep 40
-julia evaluation_svae_px_score.jl pima-indians &
-sleep 40
-julia evaluation_svae_px_score.jl sonar &
-sleep 40
-julia evaluation_svae_px_score.jl statlog-satimage &
-sleep 40
-julia evaluation_svae_px_score.jl statlog-segment &
-sleep 40
-julia evaluation_svae_px_score.jl waveform-1 &
-sleep 40
-julia evaluation_svae_px_score.jl waveform-2 &
-sleep 40
-julia evaluation_svae_px_score.jl yeast &
-sleep 40
-julia evaluation_svae_px_score.jl ionosphere &
+#julia evaluation_svae_anom.jl statlog-segment &
+#sleep 40
+#julia evaluation_svae_anom.jl waveform-1 &
+#sleep 40
+#julia evaluation_svae_anom.jl waveform-2 &
+#sleep 40
+#julia evaluation_svae_anom.jl yeast &
+#sleep 40
+julia evaluation_svae_anom.jl ionosphere &
 # sleep 40
 
