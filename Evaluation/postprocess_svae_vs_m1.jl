cca6f9fb6be8631248e9ca1aa4829df8c1c59e05
honzabim <honzabim@gmail.com>
2018-11-06 09:41:59 +0100

    postprocess m1 comp and better script

---
+++ Evaluation/postprocess_svae_vs_m1.jl
@@ -0,0 +1,48 @@
+using JLD2
+using FileIO
+using DataFrames
+using CSV
+using UCI
+
+const dataFolder = "D:/dev/julia/OSL/experiments/SVAEvsM1/"
+
+loadExperiment(filePath) = load(filePath)["results"]
+
+datasets = ["abalone", "blood-transfusion", "breast-cancer-wisconsin", "cardiotocography", "ecoli", "gisette", "glass", "haberman", "ionosphere", "iris", "isolet", "letter-recognition", "libras", "madelon", "magic-telescope", "miniboone", "multiple-features", "musk-2", "page-blocks", "pendigits", "pima-indians", "sonar", "spect", "statlog-satimage", "statlog-segment", "statlog-shuttle", "statlog-vehicle", "synthetic-control-chart", "vertebral-column", "wall-following-robot", "waveform-1", "waveform-2", "yeast", "wine"]
+model = "svae"
+
+params = [:hidden, :latent, :layers, :nonlinearity, :layertype, :β, :method, :anomaliesSeen, :auc, :i, :model, :dataset]
+types = [Int, Int, Int, String, String, Float64, String, Int, Float64, Int, String, String]
+
+function processFile!(dataframe, model, dataset)
+    for i in 1:10
+        println("Processing $model $dataset $i")
+        filename = dataFolder * dataset * "-$i-" * model * ".jld2"
+        if !isfile(filename)
+            println("$filename not found.")
+        else
+            results = loadExperiment(filename)
+            for i in 1:length(results)
+                for j in 1:length(results[1][2])
+                    pars = length(results[1][1]) > 5 ? vcat(results[i][1][1:6]...) : vcat(results[i][1]..., -1, -1)
+                    # push!(dataframe, vcat(pars..., results[i][2][ac][1:3]..., model, dataset))
+                    push!(dataframe, vcat(pars..., results[i][2][j][1:3]..., results[i][2][j][5]..., model, dataset))
+                end
+            end
+        end
+    end
+end
+
+allData = DataFrame(types, params, 0)
+for d in datasets
+    _, normal_labels, anomaly_labels = UCI.get_umap_data(d)
+    if normal_labels == nothing
+        processFile!(allData, model, d * " ")
+    else
+        for class in unique(anomaly_labels)
+            processFile!(allData, model, d * " " * "$(normal_labels[1])-$(class)")
+        end
+    end
+end
+
+CSV.write(dataFolder * "results.csv", allData)
--- Evaluation/runcomp.sh
+++ Evaluation/runcomp.sh
@@ -1,51 +1,37 @@
-julia evaluation_svae_vs_m1.jl abalone &
-julia evaluation_svae_vs_m1.jl blood-transfusion &
-julia evaluation_svae_vs_m1.jl breast-cancer-wisconsin &
-# sleep 40
-julia evaluation_svae_vs_m1.jl cardiotocography &
-# sleep 40
-julia evaluation_svae_vs_m1.jl ecoli
-# sleep 40
-julia evaluation_svae_vs_m1.jl gisette &
-julia evaluation_svae_vs_m1.jl glass &
-# sleep 40
-julia evaluation_svae_vs_m1.jl haberman &
-julia evaluation_svae_vs_m1.jl ionosphere &
-julia evaluation_svae_vs_m1.jl iris
 julia evaluation_svae_vs_m1.jl isolet &
 julia evaluation_svae_vs_m1.jl letter-recognition &
 julia evaluation_svae_vs_m1.jl libras &
-julia evaluation_svae_vs_m1.jl madelon
-julia evaluation_svae_vs_m1.jl magic-telescope &
-# sleep 40
-julia evaluation_svae_vs_m1.jl miniboone &
 julia evaluation_svae_vs_m1.jl multiple-features &
-julia evaluation_svae_vs_m1.jl musk-2
-# sleep 40
-julia evaluation_svae_vs_m1.jl page-blocks &
-# sleep 40
 julia evaluation_svae_vs_m1.jl pendigits &
-# sleep 40
-julia evaluation_svae_vs_m1.jl pima-indians &
-# sleep 40
-julia evaluation_svae_vs_m1.jl sonar
-julia evaluation_svae_vs_m1.jl spect &
-# sleep 40
+julia evaluation_svae_vs_m1.jl yeast &
+julia evaluation_svae_vs_m1.jl cardiotocography &
+julia evaluation_svae_vs_m1.jl ecoli
+julia evaluation_svae_vs_m1.jl page-blocks &
 julia evaluation_svae_vs_m1.jl statlog-satimage &
-# sleep 40
 julia evaluation_svae_vs_m1.jl statlog-segment
 julia evaluation_svae_vs_m1.jl statlog-shuttle &
 julia evaluation_svae_vs_m1.jl statlog-vehicle &
+julia evaluation_svae_vs_m1.jl breast-tissue &
 julia evaluation_svae_vs_m1.jl synthetic-control-chart
-julia evaluation_svae_vs_m1.jl vertebral-column &
 julia evaluation_svae_vs_m1.jl wall-following-robot &
-# sleep 40
-julia evaluation_svae_vs_m1.jl waveform-1
-# sleep 40
+julia evaluation_svae_vs_m1.jl waveform-1 &
 julia evaluation_svae_vs_m1.jl waveform-2 &
-# sleep 40
 julia evaluation_svae_vs_m1.jl wine &
-julia evaluation_svae_vs_m1.jl yeast &
-# sleep 40
-julia evaluation_svae_vs_m1.jl ionosphere &
-# sleep 40
+julia evaluation_svae_vs_m1.jl iris
+
+julia evaluation_svae_vs_m1.jl abalone &
+julia evaluation_svae_vs_m1.jl blood-transfusion &
+julia evaluation_svae_vs_m1.jl breast-cancer-wisconsin &
+julia evaluation_svae_vs_m1.jl gisette &
+julia evaluation_svae_vs_m1.jl glass &
+julia evaluation_svae_vs_m1.jl haberman &
+julia evaluation_svae_vs_m1.jl ionosphere
+julia evaluation_svae_vs_m1.jl madelon &
+julia evaluation_svae_vs_m1.jl magic-telescope &
+julia evaluation_svae_vs_m1.jl miniboone &
+julia evaluation_svae_vs_m1.jl musk-2 &
+julia evaluation_svae_vs_m1.jl parkinsons &
+julia evaluation_svae_vs_m1.jl pima-indians &
+julia evaluation_svae_vs_m1.jl sonar &
+julia evaluation_svae_vs_m1.jl spect-heart &
+julia evaluation_svae_vs_m1.jl vertebral-column &
