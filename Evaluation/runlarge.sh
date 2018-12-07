84dfe15822490f1fb466df6a786cd4c9a69fcf72
honzabim <honzabim@gmail.com>
2018-12-07 15:46:17 +0100

    added run script

--- Evaluation/runlarge.sh
+++ Evaluation/runlarge.sh
@@ -1,33 +1,32 @@
-julia evaluation_svae_anom.jl breast-cancer-wisconsin &
+julia evaluation_svae_vamp.jl breast-cancer-wisconsin &
 sleep 40
-julia evaluation_svae_anom.jl cardiotocography &
+julia evaluation_svae_vamp.jl cardiotocography &
 sleep 40
-#julia evaluation_svae_anom.jl ecoli &
-#sleep 40
-#julia evaluation_svae_anom.jl glass &
-#sleep 40
-# julia evaluation_svae_anom.jl magic-telescope &
+julia evaluation_svae_vamp.jl ecoli &
+sleep 40
+julia evaluation_svae_vamp.jl glass &
+sleep 40
+# julia evaluation_svae_vamp.jl magic-telescope &
 # sleep 40
-julia evaluation_svae_anom.jl musk-2 &
+julia evaluation_svae_vamp.jl musk-2 &
 sleep 40
-julia evaluation_svae_anom.jl page-blocks &
+julia evaluation_svae_vamp.jl page-blocks &
 sleep 40
-#julia evaluation_svae_anom.jl pendigits &
+#julia evaluation_svae_vamp.jl pendigits &
 #sleep 40
-#julia evaluation_svae_anom.jl pima-indians &
+#julia evaluation_svae_vamp.jl pima-indians &
 #sleep 40
-#julia evaluation_svae_anom.jl sonar &
-#sleep 40
-julia evaluation_svae_anom.jl statlog-satimage &
+julia evaluation_svae_vamp.jl sonar &
 sleep 40
-#julia evaluation_svae_anom.jl statlog-segment &
+julia evaluation_svae_vamp.jl statlog-satimage &
+sleep 40
+#julia evaluation_svae_vamp.jl statlog-segment &
 #sleep 40
-#julia evaluation_svae_anom.jl waveform-1 &
+julia evaluation_svae_vamp.jl waveform-1 &
+sleep 40
+#julia evaluation_svae_vamp.jl waveform-2 &
 #sleep 40
-#julia evaluation_svae_anom.jl waveform-2 &
-#sleep 40
-#julia evaluation_svae_anom.jl yeast &
-#sleep 40
-julia evaluation_svae_anom.jl ionosphere &
+julia evaluation_svae_vamp.jl yeast &
+sleep 40
+julia evaluation_svae_vamp.jl ionosphere &
 # sleep 40
-
