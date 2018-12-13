6039a4eaaebab6481519c256ce30851d942330b1
honzabim <honzabim@gmail.com>
2018-12-13 15:05:50 +0100

    ready for the server run

--- Evaluation/runlarge.sh
+++ Evaluation/runlarge.sh
@@ -1,32 +1,32 @@
-julia evaluation_svae_vamp.jl breast-cancer-wisconsin &
+julia evaluation_svae_prob_score.jl breast-cancer-wisconsin &
 sleep 40
-julia evaluation_svae_vamp.jl cardiotocography &
+julia evaluation_svae_prob_score.jl cardiotocography &
 sleep 40
-julia evaluation_svae_vamp.jl ecoli &
+julia evaluation_svae_prob_score.jl ecoli &
 sleep 40
-julia evaluation_svae_vamp.jl glass &
+julia evaluation_svae_prob_score.jl glass &
 sleep 40
-# julia evaluation_svae_vamp.jl magic-telescope &
+# julia evaluation_svae_prob_score.jl magic-telescope &
 # sleep 40
-julia evaluation_svae_vamp.jl musk-2 &
+julia evaluation_svae_prob_score.jl musk-2 &
 sleep 40
-julia evaluation_svae_vamp.jl page-blocks &
+julia evaluation_svae_prob_score.jl page-blocks &
 sleep 40
-#julia evaluation_svae_vamp.jl pendigits &
+julia evaluation_svae_prob_score.jl pendigits &
+sleep 40
+#julia evaluation_svae_prob_score.jl pima-indians &
 #sleep 40
-#julia evaluation_svae_vamp.jl pima-indians &
+julia evaluation_svae_prob_score.jl sonar &
+sleep 40
+julia evaluation_svae_prob_score.jl statlog-satimage &
+sleep 40
+julia evaluation_svae_prob_score.jl statlog-segment &
+sleep 40
+julia evaluation_svae_prob_score.jl waveform-1 &
+sleep 40
+#julia evaluation_svae_prob_score.jl waveform-2 &
 #sleep 40
-julia evaluation_svae_vamp.jl sonar &
+julia evaluation_svae_prob_score.jl yeast &
 sleep 40
-julia evaluation_svae_vamp.jl statlog-satimage &
-sleep 40
-#julia evaluation_svae_vamp.jl statlog-segment &
-#sleep 40
-julia evaluation_svae_vamp.jl waveform-1 &
-sleep 40
-#julia evaluation_svae_vamp.jl waveform-2 &
-#sleep 40
-julia evaluation_svae_vamp.jl yeast &
-sleep 40
-julia evaluation_svae_vamp.jl ionosphere &
+julia evaluation_svae_prob_score.jl ionosphere &
 # sleep 40
