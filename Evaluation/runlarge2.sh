31a5fb1ef679ecbfa44dfdb56bc091eb3a1cade7
honzabim <honzabim@gmail.com>
2018-12-25 12:40:13 +0100

    second half of datasets

---
+++ Evaluation/runlarge2.sh
@@ -0,0 +1,27 @@
+julia evaluation_svae_prob_score.jl blood-transfusion &
+sleep 40
+julia evaluation_svae_prob_score.jl breast-tissue &
+sleep 40
+julia evaluation_svae_prob_score.jl haberman &
+sleep 40
+julia evaluation_svae_prob_score.jl iris &
+sleep 40
+julia evaluation_svae_prob_score.jl isolet &
+sleep 40
+julia evaluation_svae_prob_score.jl miniboone &
+sleep 40
+julia evaluation_svae_prob_score.jl multiple-features &
+sleep 40
+julia evaluation_svae_prob_score.jl parkinsons &
+sleep 40
+julia evaluation_svae_prob_score.jl spect-heart &
+sleep 40
+julia evaluation_svae_prob_score.jl statlog-shuttle &
+sleep 40
+julia evaluation_svae_prob_score.jl statlog-vehicle &
+sleep 40
+julia evaluation_svae_prob_score.jl synthetic-control-chart &
+sleep 40
+julia evaluation_svae_prob_score.jl wall-following-robot &
+sleep 40
+julia evaluation_svae_prob_score.jl wine &
