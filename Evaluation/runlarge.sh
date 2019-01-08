fe2dfb857682a4d29cbd3728484f835f9f99e173
honzabim <honzabim@gmail.com>
2019-01-08 09:34:20 +0100

    New runs for evaluation svae_anom

--- Evaluation/runlarge.sh
+++ Evaluation/runlarge.sh
@@ -1,3 +1,13 @@
+julia evaluation_svae_anom.jl abalone &
+sleep 40
+julia evaluation_svae_anom.jl blood-transfusion &
+sleep 40
+julia evaluation_svae_anom.jl breast-tissue &
+sleep 40
+julia evaluation_svae_anom.jl haberman &
+sleep 40
+julia evaluation_svae_anom.jl iris &
+sleep 40
 julia evaluation_svae_anom.jl breast-cancer-wisconsin &
 sleep 40
 julia evaluation_svae_anom.jl cardiotocography &
@@ -6,21 +16,11 @@ julia evaluation_svae_anom.jl ecoli &
 sleep 40
 julia evaluation_svae_anom.jl glass &
 sleep 40
-julia evaluation_svae_anom.jl magic-telescope &
-sleep 40
-julia evaluation_svae_anom.jl musk-2 &
-sleep 40
-julia evaluation_svae_anom.jl page-blocks &
-sleep 40
-julia evaluation_svae_anom.jl pendigits &
-sleep 40
 julia evaluation_svae_anom.jl pima-indians &
 sleep 40
 julia evaluation_svae_anom.jl sonar &
 sleep 40
 julia evaluation_svae_anom.jl statlog-satimage &
-sleep 40
-julia evaluation_svae_anom.jl statlog-segment &
 sleep 40
 julia evaluation_svae_anom.jl waveform-1 &
 sleep 40
@@ -29,4 +29,5 @@ sleep 40
 julia evaluation_svae_anom.jl yeast &
 sleep 40
 julia evaluation_svae_anom.jl ionosphere &
-# sleep 40
+sleep 40
+julia evaluation_svae_anom.jl wine &
--- Evaluation/runlarge2.sh
+++ Evaluation/runlarge2.sh
@@ -1,29 +1,27 @@
-julia evaluation_svae_anom.jl abalone &
-sleep 40
-julia evaluation_svae_anom.jl blood-transfusion &
-sleep 40
-julia evaluation_svae_anom.jl breast-tissue &
-sleep 40
-julia evaluation_svae_anom.jl haberman &
-sleep 40
-julia evaluation_svae_anom.jl iris &
-sleep 40
+julia evaluation_svae_anom.jl magic-telescope &
+sleep 100
+julia evaluation_svae_anom.jl page-blocks &
+sleep 100
+julia evaluation_svae_anom.jl pendigits &
+sleep 100
 julia evaluation_svae_anom.jl isolet &
-sleep 40
+sleep 100
 julia evaluation_svae_anom.jl miniboone &
-sleep 40
+sleep 100
 julia evaluation_svae_anom.jl multiple-features &
-sleep 40
+sleep 100
 julia evaluation_svae_anom.jl parkinsons &
-sleep 40
+sleep 100
 julia evaluation_svae_anom.jl spect-heart &
-sleep 40
+sleep 100
 julia evaluation_svae_anom.jl statlog-shuttle &
-sleep 40
+sleep 100
 julia evaluation_svae_anom.jl statlog-vehicle &
-sleep 40
+sleep 100
 julia evaluation_svae_anom.jl synthetic-control-chart &
-sleep 40
+sleep 100
 julia evaluation_svae_anom.jl wall-following-robot &
-sleep 40
-julia evaluation_svae_anom.jl wine &
+sleep 100
+julia evaluation_svae_anom.jl statlog-segment &
+sleep 100
+julia evaluation_svae_anom.jl musk-2 &
