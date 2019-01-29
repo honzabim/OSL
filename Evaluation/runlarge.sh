132ce53baff03c75d2f0f45742a84fea88927069
honzabim <honzabim@gmail.com>
2019-01-29 19:50:53 +0100

    added script to run on the server

--- Evaluation/runlarge.sh
+++ Evaluation/runlarge.sh
@@ -1,5 +1,5 @@
-# julia evaluation_svae_vs_td.jl abalone &
-# sleep 40
+julia evaluation_svae_vs_td.jl abalone &
+sleep 40
 # julia evaluation_svae_vs_td.jl blood-transfusion &
 # sleep 40
 # julia evaluation_svae_vs_td.jl breast-tissue &
@@ -12,22 +12,22 @@ julia evaluation_svae_vs_td.jl breast-cancer-wisconsin &
 sleep 40
 julia evaluation_svae_vs_td.jl cardiotocography &
 sleep 40
-# julia evaluation_svae_vs_td.jl ecoli &
-# sleep 40
-# julia evaluation_svae_vs_td.jl glass &
-# sleep 40
-# julia evaluation_svae_vs_td.jl pima-indians &
-# sleep 40
-julia evaluation_svae_vs_td.jl sonar &
+julia evaluation_svae_vs_td.jl ecoli &
 sleep 40
+julia evaluation_svae_vs_td.jl glass &
+sleep 40
+julia evaluation_svae_vs_td.jl pima-indians &
+sleep 40
+# julia evaluation_svae_vs_td.jl sonar &
+# sleep 40
 julia evaluation_svae_vs_td.jl statlog-satimage &
 sleep 40
-# julia evaluation_svae_vs_td.jl waveform-1 &
-# sleep 40
+julia evaluation_svae_vs_td.jl waveform-1 &
+sleep 40
 julia evaluation_svae_vs_td.jl waveform-2 &
 sleep 40
-# julia evaluation_svae_vs_td.jl yeast &
-# sleep 40
+julia evaluation_svae_vs_td.jl yeast &
+sleep 40
 # julia evaluation_svae_vs_td.jl ionosphere &
 # sleep 40
 # julia evaluation_svae_vs_td.jl wine &
